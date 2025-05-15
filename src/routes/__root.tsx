41a553e7a04216f6a9c9368bc659309c809b5049
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2025-05-15 19:56:51 +0300

    Refactor layout structure in __root.tsx for improved styling and organization

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,17 +4,20 @@ import { FaHome } from 'react-icons/fa';
 
 export const Route = createRootRoute({
   component: () => (
-    <div className="bg-gray-900 text-gray-50">
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold flex items-center gap-1">
+    <div className="bg-gray-900 text-gray-50 h-screen flex flex-col">
+      <nav className="p-2 flex gap-2">
+        <Link to="/" className="flex items-center gap-1">
           <FaHome /> Home
         </Link>{' '}
-        <Link to="/slide1" className="[&.active]:font-bold">
-          Presentation
-        </Link>
-      </div>
+        <Link to="/slide1">Presentation</Link>
+      </nav>
+
       <hr />
-      <Outlet />
+
+      <div className="p-6 m-6 bg-gray-800 rounded-lg flex-grow">
+        <Outlet />
+      </div>
+
       <TanStackRouterDevtools position="bottom-right" />
     </div>
   ),
