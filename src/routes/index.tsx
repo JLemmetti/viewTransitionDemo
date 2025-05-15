8e8b2c89e55a313acf9ca2f5dee663ab9f00dd7a
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2025-05-15 20:53:31 +0300

    Add TampereJS logo and update welcome message in index route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router';
+import tamperejsLogo from '../assets/tamperejs.png';
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -6,8 +7,10 @@ export const Route = createFileRoute('/')({
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <div className="flex flex-col items-center justify-center min-h-screen space-y-25">
+      <h3 className="text-6xl font-bold text-center">Welcome to TampereJS!</h3>
+      <img src={tamperejsLogo} alt="TampereJS Logo" className="w-32 h-32" />
+      <p className="text-3xl text-center">Cirit / 22.5.2025</p>
     </div>
   );
 }
