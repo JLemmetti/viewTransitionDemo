ec39c02e06cc234e54029f9a2e6a46ff53259660
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2025-05-17 10:55:49 +0300

    Enhance router configuration by explicitly setting defaultViewTransition in createRouter

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,10 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { routeTree } from './routeTree.gen';
 
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  defaultViewTransition: true,
+});
 
 declare module '@tanstack/react-router' {
   interface Register {
