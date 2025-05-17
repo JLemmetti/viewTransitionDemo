49010cb3aca0a934838d4e1df50e75dd753d28c2
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2025-05-17 11:36:04 +0300

    Use ViewTransition with logo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
+import tamperejsLogo from '@assets/tamperejs.png';
 import { createFileRoute } from '@tanstack/react-router';
-import tamperejsLogo from '../assets/tamperejs.png';
+import { unstable_ViewTransition as ViewTransition } from 'react';
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -8,8 +9,17 @@ export const Route = createFileRoute('/')({
 function Index() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen space-y-25">
-      <h3 className="text-6xl font-bold text-center">Welcome to TampereJS!</h3>
-      <img src={tamperejsLogo} alt="TampereJS Logo" className="w-32 h-32" />
+      <h1 className="text-6xl font-bold text-center">Welcome to TampereJS!</h1>
+
+      <ViewTransition name="tjsLogo">
+        <img
+          src={tamperejsLogo}
+          alt="TampereJS Logo"
+          className="w-32 h-32"
+          style={{ viewTransitionName: 'tjsLogo' }}
+        />
+      </ViewTransition>
+
       <p className="text-3xl text-center">Cirit / 22.5.2025</p>
     </div>
   );
