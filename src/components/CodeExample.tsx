54773740ea1483c02976f29f32ad0c996bd8e7c7
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2025-05-21 18:46:06 +0300

    style: enhance CodeExample component with additional styling and improved layout

--- src/components/CodeExample.tsx
+++ src/components/CodeExample.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 interface CodeExampleProps {
   children: string;
+  className?: string;
 }
 
-const CodeExample: React.FC<CodeExampleProps> = ({ children }) => {
+const CodeExample: React.FC<CodeExampleProps> = ({ children, className }) => {
   return (
-    <pre className="bg-gray-900 p-4 rounded-md overflow-auto">
-      <code className="text-md">{children}</code>
+    <pre
+      className={`bg-gray-900 py-4 px-6 rounded-md overflow-auto self-center ${className}`}
+    >
+      <code className="text-xl">{children}</code>
     </pre>
   );
 };
